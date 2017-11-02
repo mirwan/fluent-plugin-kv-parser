6451dda281b98687d794a9c99ebc4afa6c0b8058
Tobias Macey <tmacey@mit.edu>
2017-11-02 11:04:36 -0400

    Manually applied changes to allow for timestamp parsing

--- lib/fluent/plugin/parser_kv.rb
+++ lib/fluent/plugin/parser_kv.rb
@@ -7,13 +7,14 @@ module Fluent::Plugin
       config_param :kv_delimiter, :string, :default => '\t\s'
       config_param :kv_char, :string, :default => '='
       config_param :time_key, :string, :default => 'time'
+      config_param :time_format, :string, :default => nil
 
       def configure(conf={})
         super
         if @kv_delimiter[0] == '/' and @kv_delimiter[-1] == '/'
           @kv_delimiter = Regexp.new(@kv_delimiter[1..-2])
         end
-
+        @time_parser = TimeParser.new(@time_format)
         @kv_regex_str = '("(?:(?:\\\.|[^"])*)"|(?:[^' + @kv_delimiter + ']*))\s*' + @kv_char + '\s*("(?:(?:\\\.|[^"])*)"|(?:[^' + @kv_delimiter + ']*))'
         @kv_regex = Regexp.new(@kv_regex_str)
       end
@@ -32,7 +33,10 @@ module Fluent::Plugin
         if time.nil?
           time = Fluent::Engine.now
         elsif time.respond_to?(:to_i)
-          time = time.to_i
+          time = @time_parser.parse(time)
+          if time.to_i < 0
+            raise RuntimeError, "The #{@time_key}=#{time} is a bad time field"
+          end
         else
           raise RuntimeError, "The #{@time_key}=#{time} is a bad time field"
         end
